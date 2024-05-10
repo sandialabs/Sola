6da8c1524f3ea8d723e92d0e41d6d83c31086e5f
Joseph Lee Hart <joshart@hpws0259.sandia.gov>
2024-05-10 08:10:11 -0600

    script for HdsaLib testing

---
+++ tests/model_discrepancy/model_discrepancy_synthetic_test_with_gsvd/Prepare_Data_For_HdsaLib_Comparison.m
@@ -0,0 +1,53 @@
+%%
+clear;
+close all;
+addpath(genpath('../../../src'));
+rng(121234);
+
+random_numbers = randn(10^5, 1);
+writematrix(random_numbers, 'random_numbers.txt');
+
+rng(121234);
+
+m = 51;
+x = linspace(0, 1, m)';
+
+data_interface = MD_Data_Interface_synthetic_test_with_gsvd(m);
+data_interface.Load_Data();
+
+alpha_u = 1 / (2^2);
+alpha_z = 1 / (100^2);
+u_prior_interface = MD_Elliptic_u_Prior_Interface_synthetic_test_with_gsvd(alpha_u, m);
+z_prior_interface = MD_Elliptic_z_Prior_Interface_synthetic_test_with_gsvd(alpha_z, m);
+
+num_prior_samples = 100;
+md_prior_sampling = MD_Prior_Sampling(data_interface, u_prior_interface, z_prior_interface);
+
+%%
+prior_delta_z_opt = md_prior_sampling.Prior_Discrepancy_Samples_at_z_opt(num_prior_samples);
+
+z = zeros(m, 3);
+z(:, 1) = x;
+z(:, 2) = x.^2 + 1;
+z(:, 3) = sin(2 * pi * x);
+prior_delta = md_prior_sampling.Prior_Discrepancy_Samples(z, num_prior_samples);
+
+%%
+md_post_sampling = MD_Posterior_Sampling(data_interface, u_prior_interface, z_prior_interface);
+alpha_d = 1.e-5;
+num_post_samples = 100;
+md_post_sampling.Compute_Posterior_Data(alpha_d, num_post_samples);
+Z_test = randn(m, 3);
+Z_test(:, 1:2) = md_post_sampling.post_data.Z;
+Z_test(:, 3) = 1.5 * ones(m, 1);
+[post_delta_mean, post_delta_samples]= md_post_sampling.Posterior_Discrepancy_Samples(Z_test);
+
+%%
+opt_prob_interface = MD_Opt_Prob_Interface_synthetic_test_with_gsvd(m);
+md_hessian_analysis = MD_Hessian_Analysis(opt_prob_interface, z_prior_interface);
+md_update = MD_Update(md_post_sampling, md_hessian_analysis);
+[post_z_mean, post_z_samples] = md_update.Posterior_Update_Samples();
+
+%%
+post_delta_mean = reshape(cell2mat(post_delta_mean), 51, 3);
+save('Sabl_Output.mat', 'prior_delta', 'prior_delta_z_opt', 'post_delta_mean', 'post_delta_samples', 'post_z_mean', 'post_z_samples');
