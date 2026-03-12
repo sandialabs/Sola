2983b53cc9496bfddc3bccbecea97fa7cebf4e67
Madhusudan Madhavan <mmadhav@sandia.gov>
2026-03-12 15:24:39 -0400

    Initialized continuation for samples

--- src/model_discrepancy/analysis/posterior/MD_Continuation_Update.m
+++ src/model_discrepancy/analysis/posterior/MD_Continuation_Update.m
@@ -204,6 +204,194 @@ classdef MD_Continuation_Update < handle
             z_out = this.Apply_Discrepancy_z_Jacobian_transpose(u_in, 1.0);
         end
 
+        % -------------------------------------------
+        % IN PROGRESS: Continuation for samples
+        % -------------------------------------------
+
+        % NOT UPDATED YET....
+        function [u, z, beta] = Posterior_Update_Samples(this)
+            u = zeros(length(this.u_opt), this.num_continuation_steps + 1);
+            z = zeros(length(this.z_opt), this.num_continuation_steps + 1);
+            t = linspace(0, 1, this.num_continuation_steps + 1);
+            beta = zeros(length(this.md_hessian_analysis.evals), this.num_continuation_steps + 1);
+
+            u(:, 1) = this.u_opt;
+            z(:, 1) = this.z_opt;
+
+            for k = 1:this.num_continuation_steps
+                % Predictive step for beta
+                Btheta_n = this.Apply_B_beta_Samples(u(:, k), beta(:, k), t(k));
+                beta_pert = -this.Apply_Parameterized_RS_Hessian_Inverse_beta_Samples(Btheta_n, u(:, k), beta(:, k), t(k));
+                beta_pred = beta(:, k) + this.step_size * beta_pert;
+
+                % Predictive step for z and u
+                z_pred = this.z_opt + this.md_hessian_analysis.evecs * beta_pred;
+                u_pred = this.opt_prob_interface.State_Solve(z_pred);
+
+                % Corrective step for beta
+                Jbeta_val = this.md_hessian_analysis.evecs' * this.Gradient_J_z_Samples(u_pred, z_pred, t(k + 1));
+                beta(:, k + 1) = beta_pred - this.Apply_Parameterized_RS_Hessian_Inverse_beta_Samples(Jbeta_val, u_pred, beta_pred, t(k + 1));
+
+                % Corrective step for z and u
+                z(:, k + 1) = this.z_opt + this.md_hessian_analysis.evecs * beta(:, k + 1);
+                u(:, k + 1) = this.opt_prob_interface.State_Solve(z(:, k + 1));
+            end
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [val, grad] = Jhat_Posterior_beta_Samples(this, beta)
+            z = this.z_opt + this.md_hessian_analysis.evecs * beta;
+            [val, J_grad_z] = this.Jhat_Posterior_Samples(z);
+            grad = this.md_hessian_analysis.evecs' * J_grad_z;
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [val, grad] = Jhat_Posterior_Samples(this, z)
+            u = this.opt_prob_interface.State_Solve(z);
+            delta = this.Discrepancy_Evaluation_Samples(z, 1);
+
+            [val, grad_u, grad_z] = this.opt_prob_interface.Objective_Function(u + delta, z);
+            z_tmp1 = this.Apply_Discrepancy_z_Jacobian_transpose_Samples(grad_u, 1);
+            z_tmp2 = this.opt_prob_interface.Apply_Solution_Operator_z_Jacobian_Transpose(grad_u, z);
+            grad = grad_z + z_tmp1 + z_tmp2;
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [Btheta_n] = Apply_B_beta_Samples(this, u_n, beta_n, t_n)
+            z_n = this.z_opt + this.md_hessian_analysis.evecs * beta_n;
+            Btheta_n = this.md_hessian_analysis.evecs' * this.Apply_B_Samples(u_n, z_n, t_n);
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [beta_out] = Apply_Parameterized_RS_Hessian_Inverse_beta_Samples(this, beta_in, u_n, beta_n, t_n)
+            beta_out = 0 * beta_in;
+            for k = 1:size(beta_in, 2)
+                tol = 1.e-7;
+                max_iter = length(beta_n) + 10;
+                [beta_out(:, k), flag, relres, iter, resvec] = pcg(@(x)this.Apply_Parameterized_RS_Hessian_beta_Samples(x, u_n, beta_n, t_n), beta_in(:, k), tol, max_iter);
+                if flag ~= 0
+                    disp(['CG did not converge; flag: ', num2str(flag), ', relres: ', num2str(relres)]);
+                end
+            end
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [beta_out] = Apply_Parameterized_RS_Hessian_beta_Samples(this, beta_in, u_n, beta_n, t_n)
+            z_n = this.z_opt + this.md_hessian_analysis.evecs * beta_n;
+            beta_out = this.md_hessian_analysis.evecs' * this.Apply_Parameterized_RS_Hessian_Samples(this.md_hessian_analysis.evecs * beta_in, u_n, z_n, t_n);
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [z_out] = Gradient_J_z_Samples(this, u_n, z_n, t_n)
+            % Useful for performing finite-difference checks
+            delta = this.Discrepancy_Evaluation_Samples(z_n, t_n);
+            [~, grad_u, grad_z] = this.opt_prob_interface.Objective_Function_Samples(u_n + delta, z_n);
+            z_tmp1 = this.Apply_Discrepancy_z_Jacobian_transpose_Samples(grad_u, t_n);
+            z_tmp2 = this.opt_prob_interface.Apply_Solution_Operator_z_Jacobian_Transpose(grad_u, z_n);
+            z_out = grad_z + z_tmp1 + z_tmp2;
+        end
+
+        % NOT UPDATED YET....
+        function [Btheta_n] = Apply_B_Samples(this, u_n, z_n, t_n)
+            delta = this.Discrepancy_Evaluation_Samples(z_n, t_n);
+
+            u_tmp1 = this.Apply_Discrepancy_theta_Jacobian_Samples(z_n);
+            u_tmp2 = this.opt_prob_interface.Apply_Misfit_Hessian(u_tmp1, u_n + delta, z_n);
+            z_tmp1 = this.opt_prob_interface.Apply_Solution_Operator_z_Jacobian_Transpose(u_tmp2, z_n);
+
+            z_tmp2 = this.Apply_Discrepancy_z_Jacobian_transpose_Samples(u_tmp2, t_n);
+
+            state_grad = this.opt_prob_interface.Misfit_Gradient(u_n + delta, z_n);
+            z_tmp3 = this.Apply_Discrepancy_z_theta_Samples(state_grad);
+
+            Btheta_n = z_tmp1 + z_tmp2 + z_tmp3;
+        end
+
+        % NOT UPDATED YET....
+        function [z_out] = Apply_Parameterized_RS_Hessian_Samples(this, z_in, u_n, z_n, t_n)
+            delta = this.Discrepancy_Evaluation_Samples(z_n, t_n);
+
+            z_out = this.opt_prob_interface.Apply_RS_Hessian(z_in, z_n); % NOTE: this computes J_{zz} + S_z' * J_uu * S_z
+            z_out = z_out(:);
+
+            u_tmp1 = this.Apply_Discrepancy_z_Jacobian_Samples(z_in, t_n);
+            u_tmp2 = this.opt_prob_interface.Apply_Misfit_Hessian(u_tmp1, u_n + delta, z_n);
+            z_out = z_out + this.opt_prob_interface.Apply_Solution_Operator_z_Jacobian_Transpose(u_tmp2, z_n);
+
+            z_out = z_out + this.Apply_Discrepancy_z_Jacobian_transpose_Samples(u_tmp2, t_n);
+
+            u_tmp3 = this.opt_prob_interface.Apply_Solution_Operator_z_Jacobian(z_in, z_n);
+            u_tmp4 = this.opt_prob_interface.Apply_Misfit_Hessian(u_tmp3, u_n + delta, z_n);
+            z_out = z_out + this.Apply_Discrepancy_z_Jacobian_transpose_Samples(u_tmp4, t_n);
+        end
+
+        % NOT UPDATED YET....
+        function [u_out] = Discrepancy_Evaluation_Samples(this, z_n, t_n)
+            N = this.md_post_sampling.post_data.N;
+            u_out = 0 * this.u_opt;
+            for ell = 1:N
+                coeff = this.md_post_sampling.post_data.a_ell(ell) + z_n' * this.Mz_Wz_inv_Mz_Z_minus_z_opt(:, ell);
+                u_out = u_out + coeff * this.md_post_sampling.post_data.u_ell(:, ell);
+                for i = 1:N
+                    coeff = this.md_post_sampling.post_data.b_i_ell(i, ell) * (this.si(i) + z_n' * this.Mz_Wz_inv_Mz_yi(:, i));
+                    u_out = u_out - coeff * this.md_post_sampling.post_data.u_i_ell{i}(:, ell);
+                end
+            end
+            u_out = (t_n / this.md_post_sampling.post_data.alpha_d) * u_out;
+        end
+
+        % NOT UPDATED YET....
+        function [u_out] = Apply_Discrepancy_z_Jacobian_Samples(this, z_in, t_n)
+            N = this.md_post_sampling.post_data.N;
+            u = 0 * this.u_opt;
+            for ell = 1:N
+                coeff = this.Mz_Wz_inv_Mz_Z_minus_z_opt(:, ell)' * z_in;
+                u = u + coeff * this.md_post_sampling.post_data.u_ell(:, ell);
+                for i = 1:N
+                    coeff = this.md_post_sampling.post_data.b_i_ell(i, ell) * (this.Mz_Wz_inv_Mz_yi(:, i)' * z_in);
+                    u = u - coeff * this.md_post_sampling.post_data.u_i_ell{i}(:, ell);
+                end
+            end
+
+            u_out = t_n * (1 / this.md_post_sampling.post_data.alpha_d) * u;
+        end
+
+        % NOT UPDATED YET....
+        function [z_out] = Apply_Discrepancy_z_Jacobian_transpose_Samples(this, u_in, t_n)
+            N = this.md_post_sampling.post_data.N;
+            z = 0 * this.z_opt;
+            for ell = 1:N
+                z = z + (this.md_post_sampling.post_data.u_ell(:, ell)' * u_in) * this.Mz_Wz_inv_Mz_Z_minus_z_opt(:, ell);
+                for i = 1:N
+                    coeff = this.md_post_sampling.post_data.b_i_ell(i, ell) * (this.md_post_sampling.post_data.u_i_ell{i}(:, ell)' * u_in);
+                    vec = this.Mz_Wz_inv_Mz_yi(:, i);
+                    z = z - coeff * vec;
+                end
+            end
+
+            z_out = t_n * (1 / this.md_post_sampling.post_data.alpha_d) * z;
+        end
+
+        % NOT UPDATED YET....
+        function [u_out] = Apply_Discrepancy_theta_Jacobian_Samples(this, z_n)
+            N = this.md_post_sampling.post_data.N;
+            u_out = 0 * this.u_opt;
+            for ell = 1:N
+                coeff = this.md_post_sampling.post_data.a_ell(ell) + z_n' * this.Mz_Wz_inv_Mz_Z_minus_z_opt(:, ell);
+                u_out = u_out + coeff * this.md_post_sampling.post_data.u_ell(:, ell);
+                for i = 1:N
+                    coeff = this.md_post_sampling.post_data.b_i_ell(i, ell) * (this.si(i) + z_n' * this.Mz_Wz_inv_Mz_yi(:, i));
+                    u_out = u_out - coeff * this.md_post_sampling.post_data.u_i_ell{i}(:, ell);
+                end
+            end
+            u_out = (1 / this.md_post_sampling.post_data.alpha_d) * u_out;
+        end
+
+        % NOT UPDATED YET.... (ONLY DEPENDENCIES NEED TO BE UPDATED)
+        function [z_out] = Apply_Discrepancy_z_theta_Samples(this, u_in)
+            z_out = this.Apply_Discrepancy_z_Jacobian_transpose_Samples(u_in, 1.0);
+        end
+
         % REMOVED FUNCTIONS (Reference for translation purposes)
 
         % function [u, z] = Posterior_Update_Mean(this)
