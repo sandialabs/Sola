61c2aafcad2f795fb81f5a5fef1c2cedd8516fa3
Madhusudan Madhavan <mmadhav@sandia.gov>
2026-04-27 15:29:25 -0400

    Minor update to run_servers for NS problem

--- examples/from_publications/Transient_Fluid_Flow_Tracer_1D/python/serverside/run_server_hifi.py
+++ examples/from_publications/Transient_Fluid_Flow_Tracer_1D/python/serverside/run_server_hifi.py
@@ -4,4 +4,5 @@
 from fluid_flow_1d_hifi_eval import *
 
 from start_server import *
-app.run(host='0.0.0.0', port=5001, debug=True)
+input("Warning: This script executes functions over localhost. Are you sure you want to continue (press enter)?");
+app.run(host='127.0.0.1', port=5001, debug=True)
